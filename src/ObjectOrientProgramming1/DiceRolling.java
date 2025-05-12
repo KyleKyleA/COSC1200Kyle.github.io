5a356abdd2312e1ee19f10820e1fe5fa8fb410db
KyleAngeles <kyle.angeles@dcmail.ca>
2025-05-12 11:37:41 -0400

    finished two java exercises so far

---
+++ src/ObjectOrientProgramming1/DiceRolling.java
@@ -0,0 +1,17 @@
+package ObjectOrientProgramming1;
+import java.util.Random;
+import java.util.Scanner;
+public class DiceRolling {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+        System.out.println("Please enter the side on the dice: ");
+        int sides = input.nextInt();
+
+        //Roll the dice (a random number will generate between numbers 1 through 6)
+        int result = rand.nextInt(sides) + 1;
+
+        System.out.println("the number you rolled the dice is: " + result + "!" );
+
+    }
+}
---
+++ src/ObjectOrientProgramming1/UserNameAge.java
@@ -0,0 +1,16 @@
+package ObjectOrientProgramming1;
+import java.util.Scanner;
+public class UserNameAge {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the user's name: ");
+        String name = input.nextLine();
+
+        System.out.println("Please enter the user's age: ");
+        int age = input.nextInt();
+
+        System.out.println("My name is " + name + " and I am" + age + " years old ");
+
+    }
+}
