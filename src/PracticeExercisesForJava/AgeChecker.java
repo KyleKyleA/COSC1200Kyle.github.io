9fbafc5944a09efea9f68c87abac50154923688a
KyleAngeles <kyle.angeles@dcmail.ca>
2025-05-13 20:27:28 -0400

    change the name of the file

--- src/PracticeExercisesForJava/AgeChecker.java
+++ src/PracticeExercisesForJava/AgeChecker.java
@@ -3,18 +3,15 @@ import java.util.Scanner;
 public class AgeChecker {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter your number: ");
-        int num1 = input.nextInt();
+        System.out.println("Please enter an age: ");
+        int age = input.nextInt();
 
-        System.out.println("Please enter number: ");
-        int num2 = input.nextInt();
-
-        if (num1 > num2) {
-            System.out.println("The first number is greater than second number");
-        } else if (num2 > num1) {
-            System.out.println("the second number is greater than the first number");
-        } else {
-            System.out.println("Both numbers are equal");
+        if (age >= 18) {
+            System.out.println("Your an Adult.");
         }
+        else {
+            System.out.println("Your a minor.");
+        }
+
     }
 }
