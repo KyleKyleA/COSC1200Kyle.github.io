479f10fcd897ab83508b2f62436c722c7c2ca8b6
KyleAngeles <kyle.angeles@dcmail.ca>
2025-05-20 21:27:39 -0400

    started working on assignment 1

---
+++ src/COSC1200KyleA/BMICalculator.java
@@ -0,0 +1,15 @@
+package COSC1200KyleA;
+import java.util.Scanner;
+public class BMICalculator {
+    // the Calculation formula for this assignment Weight (lb) / height (in) ^2 ( 703
+    public static void main(String[] args) {
+
+        //Using import scanner for user input
+        Scanner input = new Scanner(System.in);
+
+        //Declartions
+        double weight = input.nextDouble();
+
+
+    }
+}
