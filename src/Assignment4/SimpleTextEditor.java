739b5ce24c90958729aca06e92485cf150081641
KyleAngeles <kyle.angeles@dcmail.ca>
2025-07-23 14:20:39 -0400

    Adding the gui Layout for this assignment and the comments to tell where i should code for each of the buttons and the the file I/0

---
+++ src/Assignment4/SimpleTextEditor.java
@@ -0,0 +1,58 @@
+package Assignment4;
+/*Region Imported Libaries*/
+
+import javax.swing.*;
+import java.awt.*;
+
+/*EndRegion Imported Libaries*/
+/*Region Function*/
+public class SimpleTextEditor {
+    public static void main(String[] args) {
+
+        /*GUI Layout*/
+        JFrame frame = new JFrame("Welcome to simple Text Editor ");
+        frame.setSize(500, 600);
+        frame.setLayout(new BorderLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        //Only one use of this Function
+        JTextArea textArea = new JTextArea();
+
+        //JScroll
+
+
+
+        //Save Button
+
+
+        //Open Button
+
+
+
+        //Exit button
+
+
+        //TextField
+
+
+        //FileChooser
+
+
+
+        //Writing file
+
+
+
+        //Reading from a file
+
+
+
+        //ActionListner for those buttons and the file
+
+
+
+
+    }
+}
+/*EndRegion Function*/
