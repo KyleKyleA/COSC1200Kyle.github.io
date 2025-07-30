5b33b64e78307c5bd7e51012f394401cdb25a849
KyleAngeles <kyle.angeles@dcmail.ca>
2025-07-30 15:28:54 -0400

    just designing the layout that i want for this program

---
+++ src/ProjectGUI/CypherDecryptGUI.java
@@ -0,0 +1,32 @@
+package ProjectGUI;
+/*
+* Author: Kyle Angeles
+* Date: 7/30/25
+* Description: Design and construct a GUI front end for the application built for assignment #2 (Cypher).
+*  The application will allow the user to enter and configure all that is required before choosing
+* to encrypt or decrypt.
+*
+* */
+
+/*REGION IMPORTS*/
+import javax.swing.*;
+
+/*ENDREGION IMPORTS*/
+
+/*REGION FUNCTION*/
+public class CypherDecryptGUI {
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+/*ENDREGION FUNCTION*/
